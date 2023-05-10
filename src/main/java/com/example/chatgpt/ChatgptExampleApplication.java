3068545307157cd220088d8a8a8ba9470c258954
yubiao <yubiao@megvii.com>
2023-05-10 17:21:11 +0800

    init maven project

---
+++ src/main/java/com/example/chatgpt/ChatgptExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptExampleApplication.class, args);
+    }
+
+}
